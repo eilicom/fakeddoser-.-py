d609b89cd43393e9e30a74d4d71ce74c72253545
eilicom <124354129+eilicom@users.noreply.github.com>
2023-02-03 13:45:56 +0330

    Create Fakeddoser.py

---
+++ Fakeddoser.py
@@ -0,0 +1,39 @@
+#دیداسر فیک نسخه 1
+#password : eilicom
+#کپی برداری و ادیت پیگرد قانونی دارد!
+
+
+#درصورت استفاده نادرست کاربر،ایلی تیم هیچ مسئولیتی را بر عهده نمیگیرد!
+#دیداسر فیک و بسیار ساده بیشتر به درد تمرین میخوره.
+#برای افراد مبتدی!
+import pyfiglet
+
+text = ('EiLiCoM')
+welcome = ('WELCOME')
+
+print('\033[32m'+pyfiglet.figlet_format(text,font='slant')+ "\n "+'\033[93m'+"_"*59+'\033[00m')
+# دیداسر فیک نسخه 1 
+# telegram : @eilicom
+
+edame = input('Please Enter Password :')
+
+if edame  == 'eilicom':
+
+
+
+
+	
+	print('\033[32m'+pyfiglet.figlet_format(welcome,font='slant')+ "\n "+'\033[93m'+"_"*59+'\033[00m')
+	
+	ip = input('enter ip : ')
+	
+else:
+	
+	print('Wroung!')
+	
+while ip != 0:
+	
+	print('Attack Sent!')
+	#دیداسر فیک نسخه 1
+# برای دانلود نسخه های بعدی وارد کانال تلگرام با نشانی 
+#t.me/eili_ch شوید
